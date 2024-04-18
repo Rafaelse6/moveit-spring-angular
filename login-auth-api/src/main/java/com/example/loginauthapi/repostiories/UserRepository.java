04dcfdbadb82cf48465362313cedd2145a141178
Rafael Santos <rafa.05125@gmail.com>
2024-04-18 10:27:14 -0300

    UserRepository

--- login-auth-api/src/main/java/com/example/loginauthapi/repostiories/UserRepository.java
+++ login-auth-api/src/main/java/com/example/loginauthapi/repostiories/UserRepository.java
@@ -1,2 +1,8 @@
-package com.example.loginauthapi.repostiories;public interface UserRepository {
+package com.example.loginauthapi.repostiories;
+
+import com.example.loginauthapi.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User,String> {
 }
